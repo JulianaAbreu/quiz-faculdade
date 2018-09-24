acb8987eb5b447fedcee3fbb502a22768ef59686
Douglas Martins <douglas.martins.m099@gmail.com>
2018-09-24 00:43:04 -0300

    implemented questions change and user interaction

---
+++ js/main.js
@@ -0,0 +1,161 @@
+const { search } = window.location;
+const urlParams = new URLSearchParams(search);
+const category = urlParams.get('category');
+
+let currentQuestion = +location.hash.slice(1) | 0;
+const questions = getQuestions(category);
+const order = sortArrIndex(questions);
+
+const questionElements = {
+    text: document.querySelector(".quiz-question h2"),
+    alternatives: document.querySelectorAll('.question-option')
+}
+const nextArrow = document.querySelector("#narrow-next");
+const prevArrow = document.querySelector("#narrow-back");
+
+const user = {
+    answers: []
+}
+
+function sortArrIndex (array) {
+    const lenght = array.length;
+    let newArr = [];
+    while(newArr.length < array.length){
+        let index = Math.floor(Math.random() * lenght);
+        if(!newArr.includes(index)) newArr.push(index);
+    }
+    return newArr;
+} 
+
+function getQuestions (category){
+    switch (category){
+        case 'movies':
+            return moviesCategory;
+        case 'series':
+            return seriesCategory;
+        case 'animes':
+            return animesCategory;
+        case 'musics':
+            return musicsCategory;
+    }
+}
+
+function renderQuestionText (index) {
+    const quest = questions[order[index]];
+    questionElements.text.textContent = quest.question;
+    questionElements.alternatives.forEach((alt, i) => {
+        alt.children[1].textContent = quest.answers[i];
+        alt.onclick = () => {
+            questionElements.alternatives.forEach((alt) => {
+                alt.classList.remove('active');
+            })
+            if(user.answers[index] === i){
+                registAnswer(undefined)
+            } else {
+                registAnswer(i);
+                alt.classList.add('active');
+            }
+        }
+    })
+}
+
+function incrementQuestionCounter(times){
+    let nextQuestion = currentQuestion;
+    if((nextQuestion += times) < questions.length) {
+        if((nextQuestion += times) >= -1){
+            currentQuestion += times;
+            prevArrow.classList.remove('disabled');
+            nextArrow.classList.remove('disabled');
+            return true;
+        }
+        else {
+            prevArrow.classList.add('disabled');
+            return false;
+        }
+    } else {
+        nextArrow.classList.add('disabled');
+        return false;
+    }
+}
+
+function renderQuestion(quest){
+    const questNumber = document.querySelector("#current-question");
+    questNumber.textContent = (quest + 1).toString().padStart('2', 0);
+    location.hash = `#${quest.toString().padStart('2', 0)}`;
+    renderQuestionText(quest);
+    if(isAnswered(quest)){
+        questionElements.alternatives[user.answers[quest]]
+            .classList.add('active');
+    }
+}
+
+function changeQuestion(forward){
+    // Increase or Decrease currentQuestion
+    // If not succeed, stop the function
+    if(!incrementQuestionCounter(forward ? +1 : -1)) return;
+    
+    // Generate an array with classes in order to be added
+    let animationClass;
+    if(forward) animationClass = ["changeOut", "changeIn"];
+    else animationClass = ["changeIn", "changeOut"];
+
+    // Remove all "active" class from alternatives
+    questionElements.alternatives.forEach((alt) => {
+        alt.classList.remove('active');
+    })
+
+    // Disable next navigation button
+    if(forward && !isAnswered(currentQuestion)) nextArrow.classList.add('disabled');
+
+    // Animate question area & change question
+    let questionArea = document.querySelector(".quiz-question");
+    if(!forward) questionArea.classList.add("changeReverse");
+    questionArea.classList.add(animationClass[0]);
+    setTimeout(() => {
+        questionArea.classList.remove(animationClass[0]);
+        questionArea.classList.add(animationClass[1]);
+        renderQuestion(currentQuestion);
+        setTimeout(() => {
+            questionArea.classList.remove(animationClass[1]);
+            if(!forward) questionArea.classList.remove("changeReverse");
+        }, 250);
+    }, 250)
+}
+
+function registAnswer(index){
+    user.answers[currentQuestion] = index;
+    nextArrow.classList.remove('disabled');
+}
+
+function isAnswered(quest){
+    return user.answers[quest] != undefined;
+}
+
+function init(){
+    // Show Question & Answers on screen
+    renderQuestionText(currentQuestion);
+    
+    // Reset user answers' array
+    user.answers.length = questions.length;
+    user.answers.fill(undefined);
+
+    // Disable navigation buttons
+    nextArrow.classList.add('disabled');
+    if(currentQuestion === 0) prevArrow.classList.add('disabled');
+
+    // Add events to navigation buttons
+    prevArrow.onclick = () => changeQuestion(false);
+    nextArrow.onclick = (evt) => {
+
+        // Only allow change question if one answer was selected
+        if(isAnswered(currentQuestion)){
+            evt.target.classList.remove('disabled');
+            changeQuestion(true);
+        } else {
+            evt.target.classList.add('disabled');
+            alert("Você precisa selecionar uma alternativa antes de continuar");
+        }
+    }
+}
+
+init();
---
+++ js/quiz-questions.js
@@ -0,0 +1,341 @@
+const moviesCategory = [
+    {
+        id: 0,
+        question: "Duas crianças crescem juntas em uma ilha junto de um velho marinheiro, após um naufrágio. Quando o velho morre, as duas crianças têm de se virar sozinhas, e acabam por descobrir o amor. Que filme é esse?",
+        answers: [
+            "Naufrágio",
+            "Lagoa azul",
+            "Pecados Íntimos",
+            "O caminho para Casa"
+        ],
+        rightAnswer: 1
+    },
+    {
+        id: 1,
+        question: "Dois policiais negros (Marlon e Shawn Wayans, irmãos de verdade) decidem se disfarçar de duas moças brancas, afim de investigar uma denúncia de seqüestro e, assim, tentar salvar seus empregos. Que filme é esse?",
+        answers: [
+            "Dois Policiais Meio Suspeitos",
+            "Loucademia de Polícia",
+            "Os irmãos Shawn",
+            "As Branquelas"
+        ],
+        rightAnswer: 3
+    },
+    {
+        id: 2,
+        question: "Rachel Keller é uma repórter que decide investigar a misteriosa morte de sua sobrinha. Ela assiste a uma fita amaldiçoada, que possivelmente teria uma ligação não só com esse caso, mas com diversas outras mortes repentinas. Ela recebe então o chamado, que a avisa de seu prazo de sete dias para reverter a maldição e, assim, impedir que a morte bata à sua porta. Que filme é esse?",
+        answers: [
+            "A Maldição",
+            "O Chamado",
+            "Uma Chamada Perdida",
+            "Seven – Os Sete Crimes Capitais"
+        ],
+        rightAnswer: 1
+    },
+    {
+        id: 3,
+        question: "Um certo rapaz teve problemas de memória quando garoto. Já crescido, ele descobre uma capacidade de poder viajar pelo tempo através desses buracos em suas lembranças. Ele utiliza desse poder para poder ficar com a garota dos seus sonhos, porém, isso o insere em um ciclo que, a cada vez que ele utiliza esse poder, algo de muito ruim acontece a alguém querido de sua vida. Evan tenta então, voltando sempre no tempo, salvar as pessoas, mas por mais que se esforce, alguém sempre vai sair perdendo. Que filme é esse?",
+        answers: [
+            "Efeito Borboleta",
+            "Efeito Dominó",
+            "O Mistério da Libélula",
+            "De Volta para o Futuro"
+        ],
+        rightAnswer: 0
+    },
+    {
+        id: 4,
+        question: "Primeira animação da Walt Disney Inc. a ser feita totalmente no computador:",
+        answers: [
+            "Toy Story",
+            "Os incríveis",
+            "Vida de Insetos",
+            "Branca de Neve e os Sete Anões"
+        ],
+        rightAnswer: 3
+    },
+    {
+        id: 5,
+        question: "Em qual filme nacional o protagonista é um capitão da polícia militar do Estado do Rio de Janeiro:",
+        answers: [
+            "Federal",
+            "Assalto ao Banco Central",
+            "Tropa de Elite",
+            "A Grande Família"
+        ],
+        rightAnswer: 2
+    },
+    {
+        id: 6,
+        question: "Em que filme Jim Carry interpreta um marido de dupla personalidade que foi abandonado pela esposa?",
+        answers: [
+            "Irene e Eu",
+            "Eu, Eu Mesmo & Irene",
+            "Eu mais Eu",
+            "Dois em um"
+        ],
+        rightAnswer: 1
+    },
+    {
+        id: 7,
+        question: "“Hasta la vista, Baby” de qual filme é esta frase?",
+        answers: [
+            "Máfia",
+            "Poderoso chefão",
+            "Crepúsculo",
+            "Exterminador do Futuro 2"
+        ],
+        rightAnswer: 3
+    }
+];
+
+const seriesCategory = [
+    {
+        id: 0,
+        question: "Qual é a série onde o personagem principal é um repórter fotográfico que trabalha para um Jornal?",
+        answers: [
+            "Batman",
+            "Lanterna Verde",
+            "Os incríveis",
+            "Homem Aranha"
+        ],
+        rightAnswer: 3
+    },
+    {
+        id: 1,
+        question: "Quantas séries dos Power Rangers já existiram?",
+        answers: [
+            "19",
+            "4",
+            "8",
+            "171"
+        ],
+        rightAnswer: 0
+    },
+    {
+        id: 2,
+        question: "Na primeira temporada de Grey's Anatomy, qual o nome da esposa de Derek?",
+        answers: [
+            "Meredith Grey",
+            "Izzie Stevens",
+            "Addison Montgomery",
+            "Cristina yang"
+        ],
+        rightAnswer: 2
+    },
+    {
+        id: 3,
+        question: "Qual o nome da atriz que protagoniza Meredith Grey em Grey's Anatomy?",
+        answers: [
+            "Jessica Capshaw",
+            "Ellen Pompeo",
+            "Sara Ramirez",
+            "Sandra Oh"
+        ],
+        rightAnswer: 1
+    },
+    {
+        id: 4,
+        question: "Qual casal adotou uma criança africana em Grey's Anatomy?",
+        answers: [
+            "Cristina e Owen",
+            "Callie e Arizona",
+            "Meredith e Derek",
+            "Ellis Grey e Richard Webber"
+        ],
+        rightAnswer: 2
+    },
+    {
+        id: 5,
+        question: "De onde Ted e Robin “vieram” (nasceram), respectivamente?",
+        answers: [
+            "Canadá e Ohio",
+            "New York e Arizona",
+            "New York e Canadá",
+            "Ohio e Canadá"
+        ],
+        rightAnswer: 2
+    },
+    {
+        id: 6,
+        question: "Que personagem costuma fazer um jogo chamado “Você conhece o Ted?”:",
+        answers: [
+            "Lily",
+            "Marshal",
+            "Barney",
+            "O próprio Ted"
+        ],
+        rightAnswer: 2
+    },
+    {
+        id: 7,
+        question: "Relacionado ao filme “Doctor Who”, quantos olhos tem um Dalek?",
+        answers: [
+            "1",
+            "2",
+            "3",
+            "4"
+        ],
+        rightAnswer: 0
+    }
+]
+
+const animesCategory = [
+    {
+        id: 0,
+        question: "Sword Art Online é uma série de TV adaptada da light novel de Reki Kawahara e tem sua 3ª temporada programada para outubro de 2018. Ela conta a história de Kirito em um futuro próximo no qual jogos e realidade estão intimamente ligados. A façanha é permitida através de um console de Realidade Virtual similar a um capacete o qual se denomina:",
+        answers: [
+            "NerveGear",
+            "NeuroLinker",
+            "AmonSphere",
+            "FullDrive"
+        ],
+        rightAnswer: 0
+    },
+    {
+        id: 1,
+        question: "\"Ano Hana\", uma abreviação para a série de anime e mangá criada por \"Mari Okada\", lançada em meados de 2011 pelo estúdio A-1 Pictures. Retrata a história de seis amigos de infância ao se reencontrarem 10 anos depois, a fim de descobrir qual o desejo que um deles precisa realizar para que consiga descansar em paz após um acidente. Qual o nome desse personagem?",
+        answers: [
+            "Yadomi Jintan",
+            "Honma \"Menma\" Meiko",
+            "Anjou Naruko",
+            "Matsuyuki \"Yukiatsu\""
+        ],
+        rightAnswer: 1
+    },
+    {
+        id: 2,
+        question: "A franquia Pokémon é um sucesso mundial e traz inúmeras recordações a todos os seus admiradores. No anime, acompanhamos a história de Ash Ketchum almejando se tornar um Mestre Pokémon e, para isso, participa de diversas Ligas Pokémon ao redor do mundo. Durante suas viagens, em quais delas Ash conseguiu capturar todos os Pokémons iniciais de sua região?",
+        answers: [
+            "Johto, Unova e Alola",
+            "Hoenn, Sinnoh e Kalos",
+            "Kanto, Johto e Unova",
+            "Kanto, Kalos e Alola"
+        ],
+        rightAnswer: 2
+    },
+    {
+        id: 3,
+        question: "\"Shingeki no Kyojin\" ou \"Ataque dos Titãs\" é uma série de anime e mangá popular no mundo todo. Retrata a vida da humanidade rodeada por muralhas após a aparição ameaçadora de criaturas de dimensões colossais: os Titãs. O anime ocorre há mais de um milênio no passado, mais precisamente em:",
+        answers: [
+            "680-685 a.C.",
+            "850-854 d.C.",
+            "420-422 a.C",
+            "700-750 d.C"
+        ],
+        rightAnswer: 1
+    },
+    {
+        id: 4,
+        question: "\"One Piece\" é uma popular série de mangá com suas adaptações para a TV exibida até hoje com mais de 800 episódios. Sua trama foca na busca do Capitão Monkey D. Luffy e sua tripulação pirata atrás do maior tesouro do mundo: o One Piece. Dentre um de seus temas de abertura, temos a canção \"Fight Together\", interpretada por Namie Amuro, sendo a ... abertura da série, lançada em 2011.",
+        answers: [
+            "Décima Primeira",
+            "Nona",
+            "Décima Quarta",
+            "Décima Terceira"
+        ],
+        rightAnswer: 2
+    },
+    {
+        id: 5,
+        question: "Angel Beats é uma série de TV baseada na Light Novel de \"Jun Maeda\", sendo uma das produções mais populares do estúdio PA Works. A série retrata a vida de pessoas que, após morrerem, se encontram em um tipo de escola que as permite encarar as dificuldades e traumas enfrentados durante a vida passada, a fim de que tenham uma segunda chance de viver. Uma das personagens, \"Tenshi\" ou \"Angel\", busca:",
+        answers: [
+            "Auxiliar as pessoas a aceitarem o que aconteceu em vida",
+            "Lutar contra Deus, depois da morte injusta de seus irmãos",
+            "Se tornar médico, depois da morte de sua irmã por um câncer",
+            "Aproveitar a vida, já que um acidente a deixou paraplégica"
+        ],
+        rightAnswer: 0
+    }
+]
+
+const musicsCategory = [
+    {
+        id: 0,
+        question: "Os Titãs é uma das bandas de rock com maior popularidade no Brasil. Com carreira iniciada em 1982, a banda já conta com mais de 20 álbuns e inúmeros prêmios em diversas categorias. “O acaso vai me proteger enquanto eu andar distraído” é o refrão de uma de suas músicas, a qual recebe o nome de: ",
+        answers: [
+            "Epitáfio",
+            "O Acaso",
+            "Enquanto Houver Sol",
+            "Sonífera Ilha"
+        ],
+        rightAnswer: 0
+    },
+    {
+        id: 1,
+        question: "Uma das séries de sucesso internacional, \"La Casa de Papel\", teve seu tema de abertura remixado recentemente pelo DJ brasileiro Alok, a canção interpretada pela cantora Cecilia Krull tem como título:",
+        answers: [
+            "I don't care at all",
+            "My Life Is Going On",
+            "If I stay with You",
+            "I'm lost"
+        ],
+        rightAnswer: 1
+    },
+    {
+        id: 2,
+        question: "Depois de mais de 5 anos sem um novo álbum, Avril Lavigne - artista responsável por sons como \"Complicated\", \"Sk8er Boi\" e \"My Happy Ending\"- pretende colocar um fim em seu hiato lançando um novo disco em breve. Dentre as músicas anunciadas está:",
+        answers: [
+            "Keep me Safe",
+            "Let Me Go (2nd Version)",
+            "Head Above Water",
+            "Goodbye"
+        ],
+        rightAnswer: 2
+    },
+    {
+        id: 3,
+        question: "O segundo álbum da dupla sertaneja Zé Neto & Cristiano intitulado \"Esquece o mundo lá fora\", aclamado por muitos fãs, foi lançado em maio de 2018, sua gravação ao-vivo foi realizada durante show na cidade de:",
+        answers: [
+            "Cuiabá/MT",
+            "São Paulo/SP",
+            "Salvador/BA",
+            "Goiania/GO"
+        ],
+        rightAnswer: 1
+    },
+    {
+        id: 4,
+        question: "\"Back to You\" é um single lançado recentemente pela cantora norte-americana Selena Gomez, em pouco tempo alcançou o top 40 na Billboard, entre outros fatores, devido a:",
+        answers: [
+            "ser um dos temas da Copa do Mundo Fifa 2018",
+            "participação e composição de Ian Kirkpatrick",
+            "ser tema da 2 temporada de 13 Reasons Why",
+            "aparição de Andrey Kupchenko no clipe oficial"
+        ],
+        rightAnswer: 2
+    },
+    {
+        id: 5,
+        question: "Destaque no Pop/EDM atual está o single \"Solo\" de Clean Bandit e Demi Lovato. O grupo britânico responsável por músicas como \"Synphony\", \"Rockabye\" e \"Rather Be\" é composto por três produtores:",
+        answers: [
+            "Grace Chatto, Luke Patterson & Jack Patterson",
+            "Jean Paul Makhlouf, Alex Makhlouf & Sam Frisch",
+            "Fadil El Ghoul, Florian Picasso & Mick Kastenholt",
+            "Justin David Blau, Artyom Stolyarov & Niles Hollowell-Dhar"
+        ],
+        rightAnswer: 0
+    },
+    {
+        id: 6,
+        question: "Com críticas mistas, Anitta lançou em julho de 2018 o single \"Medicina\". Em espanhol, um de seus versos traduzidos se reflete em:",
+        answers: [
+            "\"Música viciando como remédio\"",
+            "\"Eu não me importo se você tem dinheiro\"",
+            "\"Todos gostamos quando nos apreciam\"",
+            "\"Todo mundo se solta mais quando se tem luz\""
+        ],
+        rightAnswer: 1
+    },
+    {
+        id: 7,
+        question: "Will Smith é um dos artistas que compõem o cast da música tema da Copa do Mundo Fifa 2018, denominada de \"Live It Up\", o tema também conta com a participação de:",
+        answers: [
+            "Nicky Jan & Era Istrefi",
+            "Anne-Marie & Raiden",
+            "Ina Wroldsen & Luiz Fonsi",
+            "Martin Garrix & Miranda Glory"
+        ],
+        rightAnswer: 0
+    }
+]
